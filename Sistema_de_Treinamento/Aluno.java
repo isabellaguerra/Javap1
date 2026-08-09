e8f26c3c6807e6b00f11dd32daf5bae6d577ace3
Isabella <141265923+isabellaguerra@users.noreply.github.com>
2026-08-09 13:07:31 -0300

    Update Aluno.java

--- Sistema_de_Treinamento/Aluno.java
+++ Sistema_de_Treinamento/Aluno.java
@@ -1,1 +1,18 @@
+public class Aluno {
 
+    private String nome;
+    private double notaFinal;
+
+    public Aluno(String nome, double notaFinal) {
+        this.nome = nome;
+        this.notaFinal = notaFinal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+}
