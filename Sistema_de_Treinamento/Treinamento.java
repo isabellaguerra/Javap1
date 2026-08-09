8897c2ccd52b939026c6b390db61516c33f3910d
Isabella <141265923+isabellaguerra@users.noreply.github.com>
2026-08-09 13:07:57 -0300

    Update Treinamento.java

--- Sistema_de_Treinamento/Treinamento.java
+++ Sistema_de_Treinamento/Treinamento.java
@@ -1,1 +1,71 @@
+import java.util.ArrayList;
 
+public class Treinamento {
+
+    private int id;
+    private String nomeInstrutor;
+    private String linguagemEnsinada;
+    private ArrayList<Aluno> alunos;
+    private int cargaHoraria;
+
+    public Treinamento(int id, String nomeInstrutor, String linguagemEnsinada) {
+
+        this.id = id;
+        this.nomeInstrutor = nomeInstrutor;
+        this.linguagemEnsinada = linguagemEnsinada;
+        this.alunos = new ArrayList<>();
+    }
+
+    public boolean verificarDisponibilidade() {
+
+        if (nomeInstrutor != null && !nomeInstrutor.isEmpty()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void definirCargaHoraria(int horas) {
+
+        cargaHoraria = horas;
+
+        System.out.println(
+            "Carga horária definida: " + horas + " horas"
+        );
+    }
+
+    public boolean verificarUltimoTreinamento(Aluno aluno) {
+
+        if (cargaHoraria <= 80) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public double calcularMediaAlunos() {
+
+        if (alunos.isEmpty()) {
+            return 0;
+        }
+
+        double soma = 0;
+
+        for (Aluno aluno : alunos) {
+
+            soma += aluno.getNotaFinal();
+        }
+
+        return soma / alunos.size();
+    }
+
+    public void adicionarAluno(Aluno aluno) {
+
+        alunos.add(aluno);
+    }
+
+    public void mostrarTipo() {
+
+        System.out.println("Treinamento normal");
+    }
+}
