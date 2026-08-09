a27e82e88d7b07e757a6ca3f3e0e866e9afc6644
Isabella <141265923+isabellaguerra@users.noreply.github.com>
2026-08-09 13:08:22 -0300

    Add TreinamentoPresencial class for in-person training

--- Sistema_de_Treinamento/TreinamentoOnline.java
+++ Sistema_de_Treinamento/TreinamentoOnline.java
@@ -1,1 +1,26 @@
+public class TreinamentoPresencial extends Treinamento {
 
+    private String local;
+
+    public TreinamentoPresencial(
+            int id,
+            String nomeInstrutor,
+            String linguagemEnsinada,
+            String local) {
+
+        super(id, nomeInstrutor, linguagemEnsinada);
+
+        this.local = local;
+    }
+
+    public void mostrarLocal() {
+
+        System.out.println("Local: " + local);
+    }
+
+    @Override
+    public void mostrarTipo() {
+
+        System.out.println("Treinamento presencial");
+    }
+}
