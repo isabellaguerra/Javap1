fcfe95292baf56b37bc4965937a806cc37864060
Isabella <141265923+isabellaguerra@users.noreply.github.com>
2026-08-09 13:01:36 -0300

    Update ItemPedido.java

--- Sistema _Pedidos_Restaurante/ItemPedido.java
+++ Sistema _Pedidos_Restaurante/ItemPedido.java
@@ -1,1 +1,26 @@
+public class ItemPedido {
 
+    private String nomeDoPrato;
+    private int quantidade;
+    private double precoUnitario;
+
+    public ItemPedido(
+            String nomeDoPrato,
+            int quantidade,
+            double precoUnitario) {
+
+        this.nomeDoPrato = nomeDoPrato;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public double calcularSubtotal() {
+
+        return quantidade * precoUnitario;
+    }
+
+    public String getNomeDoPrato() {
+
+        return nomeDoPrato;
+    }
+}
