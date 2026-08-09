8ebee89f5d576b9057e4a4388aaba2f8290f3bd9
Isabella <141265923+isabellaguerra@users.noreply.github.com>
2026-08-09 13:08:41 -0300

    Add TreinamentoOnline class for online training

--- Sistema_de_Treinamento/TreinamentoPresencial.java
+++ Sistema_de_Treinamento/TreinamentoPresencial.java
@@ -1,1 +1,26 @@
+public class TreinamentoOnline extends Treinamento {
 
+    private String linkAcesso;
+
+    public TreinamentoOnline(
+            int id,
+            String nomeInstrutor,
+            String linguagemEnsinada,
+            String linkAcesso) {
+
+        super(id, nomeInstrutor, linguagemEnsinada);
+
+        this.linkAcesso = linkAcesso;
+    }
+
+    public void mostrarLink() {
+
+        System.out.println("Link: " + linkAcesso);
+    }
+
+    @Override
+    public void mostrarTipo() {
+
+        System.out.println("Treinamento online");
+    }
+}
